96b7cdddea18a802aebadf0b509674e7bb566b96
༒ᶜʳᵃᶻʸᴮᴼˢˢ卂乃卄丨丂卄乇Ҝ༒ <93928612+Abhishek-Issac@users.noreply.github.com>
2024-07-07 17:15:08 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,3 @@
-import asyncio
-from os import environ
-from pyrogram import Client, filters, idle, enums
-from datetime import datetime, timedelta
-from pytz import utc
-from utils import scheduler 
-
 
 API_ID = int(environ.get("API_ID"))
 API_HASH = environ.get("API_HASH")
@@ -15,61 +8,18 @@ GRP = int(environ.get("GRP"))
 
 
 
-START_MSG = """<b>Hai {}"""
-
 
 User = Client("sflixrunner",
               api_id=API_ID,
               api_hash=API_HASH, 
-              session_string=SESSION)
+              session_string=SESSION
+              plugins=dict(root=plugin))
 
-Bot = Client(name="sflixrunner",
-             api_id=API_ID,
-             api_hash=API_HASH,
-             bot_token=BOT_TOKEN,
-             workers=30
-             )
-
-
-@Bot.on_message(filters.command('start') & filters.private)
-async def start(bot, message):
-    await message.reply(START_MSG.format(message.from_user.mention))
-    
-@User.on_message(filters.group & filters.text & filters.incoming)
-async def delete(user, message):
-    if message.chat.id != GRP:
-        return
-    try:
-        msg = message
-        chat_id = msg.chat.id
-        admkns = user.get_chat_members(chat_id, filter=enums.ChatMembersFilter.ADMINISTRATORS)
-        for admin in admkns:
-            if msg.from_user.id == admin.user.id:
-             return
-                
-        await scheduler.add_job(
-            lete,
-            "date",
-            [user, msg],
-            run_date=datetime.now() + timedelta(seconds=TIME),
-        )
-    except Exception as e:
-        print(e)
-
-async def delete(user, msg):
-    try:
-        deleted = await user.delete_messages((msg.chat.id),(msg.id))
-    except Exception as e:
-        print(e)
 
 User.start()
 print("User Started!")
-Bot.start()
-print("Bot Started!")
 
 idle()
 
 User.stop()
 print("User Stopped!")
-Bot.stop()
-print("Bot Stopped!")
