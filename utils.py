c2f98e2bb6aaeed5d36dc6ae91d1920ad6ddd7d9
༒ᶜʳᵃᶻʸᴮᴼˢˢ卂乃卄丨丂卄乇Ҝ༒ <93928612+Abhishek-Issac@users.noreply.github.com>
2024-07-07 17:23:42 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,7 +2,6 @@ from pyrogram import Client
 import asyncio
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from pytz import utc
-from bot import User, Bot
 
 
 scheduler = AsyncIOScheduler(timezone="UTC")
