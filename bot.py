bcebee71adc52931a01feca1d045b0b1cd2d28d2
༒ᶜʳᵃᶻʸᴮᴼˢˢ卂乃卄丨丂卄乇Ҝ༒ <93928612+Abhishek-Issac@users.noreply.github.com>
2024-07-07 17:30:57 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,20 +11,30 @@ SESSION = environ.get("SESSION")
 TIME = int(environ.get("TIME"))
 GRP = int(environ.get("GRP"))
 
+class MyClient(Client):
 
+    def __init__(self, name_, bot_token=None, session_string=None):
+        super().__init__(
+            name=name_,
+            api_id=API_ID,
+            api_hash=API_HASH,,
+            session_string=SESSION,
+            plugins={"root": "plugin"},
+            sleep_threshold=5,
+        )
 
+    async def start(self):
+        await super().start()
+        me = await self.get_me()
+        self.username = '@' + me.username if me.username else ""
+        self.myid = me.id
+        print(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
 
-User = Client("sflixrunner",
-              api_id=API_ID,
-              api_hash=API_HASH, 
-              session_string=SESSION,
-              plugins={"root": "plugin"})
-
-
-User.start()
-print("User Started!")
-
-idle()
-
-User.stop()
-print("User Stopped!")
+    async def stop(self, *args):
+        await super().stop()
+        print("Bot stopped. Bye.")
+    
+    
+User = MyClient(name_="sflixrunnerr",
+               session_string=SESSION
+                  )
