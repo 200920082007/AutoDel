5ba97f49fa6247b22fe674839951d4d1275abe8d
༒ᶜʳᵃᶻʸᴮᴼˢˢ卂乃卄丨丂卄乇Ҝ༒ <93928612+Abhishek-Issac@users.noreply.github.com>
2024-07-07 16:57:12 +0530

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,17 @@
+from pyrogram import Client, message
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from pytz import utc
+from bot import User, Bot
+
+
+scheduler = AsyncIOScheduler(timezone="UTC")
+
+
+async def lete(user, msg):
+    try:
+        deleted = await user.delete_messages((msg.chat.id),(msg.id))
+    except Exception as e:
+        print(e)
+
+
+scheduler.start()
