042216aec3e40908ee3e7fbe818337417eabbcae
༒ᶜʳᵃᶻʸᴮᴼˢˢ卂乃卄丨丂卄乇Ҝ༒ <93928612+Abhishek-Issac@users.noreply.github.com>
2024-07-07 15:02:48 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -64,6 +64,11 @@ async def delete(user, message):
     except Exception as e:
         print(e)
 
+async def lete(user, msg):
+    try:
+        deleted = await user.delete_messages((msg.chat.id),(msg.id))
+    except Exception as e:
+        print(e)
 
 
 User.start()
